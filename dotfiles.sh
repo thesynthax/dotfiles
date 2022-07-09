4b2fa33b8df45e4ee082f167a69e23f18248981a
Kushagra Srivastava <kushagra1403@gmail.com>
2022-07-09 07:11:42 +0530

    add dotfiles

---
+++ dotfiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
+ln -sf ~/dotfiles/.xprofile ~/.xprofile
+ln -sf ~/dotfiles/.xsessionrc ~/.xsessionrc
+ln -sf ~/dotfiles/.zsh_history ~/.zsh_history
+ln -sf ~/dotfiles/.nvimrc ~/.nvimrc
+ln -sf ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
+ln -sf ~/dotfiles/.config/alacritty ~/.config/alacritty
+ln -sf ~/dotfiles/.config/betterlockscreenrc ~/.config/betterlockscreenrc
+ln -sf "~/dotfiles/.config/Code - OSS/" "~/.config/Code - OSS/"
+ln -sf ~/dotfiles/.config/i3 ~/.config/i3
+ln -sf ~/dotfiles/.config/picom ~/.config/picom
+ln -sf ~/dotfiles/.config/polybar ~/.config/polybar
