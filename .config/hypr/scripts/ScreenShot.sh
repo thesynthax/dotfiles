f27b1bf442e10fc0f90227583de716ae23d9fe4f
thesynthax <kushagra1403@gmail.com>
2026-08-05 14:24:06 +0530

    fix ss dir
    Signed-off-by: thesynthax <kushagra1403@gmail.com>

--- .config/hypr/scripts/ScreenShot.sh
+++ .config/hypr/scripts/ScreenShot.sh
@@ -7,7 +7,7 @@ sDIR="$HOME/.config/hypr/scripts"
 notify_cmd_shot="notify-send -h string:x-canonical-private-synchronous:shot-notify -u low -i ${iDIR}/picture.png"
 
 time=$(date "+%d-%b_%H-%M-%S")
-dir="$(xdg-user-dir)/pics/screenshots"
+dir="$HOME/pics/screenshots"
 file="Screenshot_${time}_${RANDOM}.png"
 
 active_window_class=$(hyprctl -j activewindow | jq -r '(.class)')
